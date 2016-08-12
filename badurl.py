0e2ffbce2996efeee6858c2c9f4dd701ae74ad80
BOBSonSeungHo <bu03083@naver.com>
2016-08-12 23:11:30 +0900

    finally updated badurl.py

--- badurl.py
+++ badurl.py
@@ -7,4 +7,33 @@ def process_tcp_packet(packet):
 
 
 
-	http_layer = packet.getlayer
+	http_layer = packet.getlayer(http.HTTPRequest)
+	IP_Layer = packet.getlayer(IP)
+
+
+
+a = 'http://[host]'. format.(IP_Layer.fields, http_layer.fields)
+
+
+	if arr.count(a) == 1 :
+	
+		fp.write('this is warning site :' ('\n')
+
+
+f = open ("/home/no_answer/BOBNETWORK/BOBNETWORK/badurl.txt", 'r')
+
+fp = open("/home/no_answer/BOBNETWORK/BOBNETWORK/logsample.txt", 'w')
+
+
+arr = []
+
+while 1 :
+		line = f.readline()
+	if not line : break
+	temp = line rstrip('\n')
+	
+
+f.close()
+
+
+sniff(filter='tcp', prn=process_tcp_packet)
